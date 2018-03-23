134dbe7f1fa995388dbfe6c2c393e301375b1c17
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2018-03-22 18:42:08 -0700

    Initial vars added to CSS, starting to add EL to JS

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -1,1 +1,8 @@
-script.js
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate(){
+	console.log(this.value);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
