d52589f6420849c4da87000af00fa0a4e7ceb912
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2018-04-30 15:50:47 -0700

    Completed checkbox shift challenge

--- 10 - Hold Shift and Check Checkboxes/script.js
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -1,1 +1,24 @@
-style.css
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e) {
+	let inBetween = false;
+
+	// check if shift key down and box checked
+	if (e.shiftKey && this.checked) {
+		checkboxes.forEach(check => {
+			if (check === this || check === lastChecked) {
+				inBetween = !inBetween;
+			}
+			if (inBetween) {
+				check.checked = true;
+			}
+		});
+	}
+
+	lastChecked = this;
+
+}
+
+checkboxes.forEach(check => check.addEventListener('click', handleCheck));
