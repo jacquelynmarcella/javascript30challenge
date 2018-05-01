a26474a9448fbcf02e308ec8a108c0959e5c9097
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2018-04-30 17:50:29 -0700

    Got video player controls working

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -11,11 +11,42 @@ const ranges = player.querySelectorAll('.player__slider');
 function togglePlay() {
 	const method = video.paused ? 'play' : 'pause';
 	video[method]();
-	toggle.textContent = video.paused ? '►' : '❚❚';
+	toggle.textContent = video.paused ? '►' : '❚ ❚';
+}
+
+function skip() {
+	video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+	video[this.name] = this.value;
+	console.log(this.value, this.name);
+}
+
+function handleProgress() {
+	const percent = (video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis = `${percent}%`;
+	console.log(percent);
+}
+
+function scrub(e){
+	const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+	video.currentTime = scrubTime;
+	console.log(e);
 }
 
 toggle.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', handleProgress);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('click', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', scrub);
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
 
 
 // event listeners
